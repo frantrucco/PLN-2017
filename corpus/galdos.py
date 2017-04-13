421f066cd68185881fd737d892a2e6c70e84edb7
Francisco Trucco <franciscoctrucco@gmail.com>
2017-04-13 07:17:51 -0300

    Split galdos corpus into training set and testing set

--- corpus/galdos.py
+++ corpus/galdos.py
@@ -2,3 +2,6 @@ from nltk.corpus import PlaintextCorpusReader
 
 galdos = PlaintextCorpusReader('corpus/', 'galdos.txt')
 """ The galdos Corpus Reader initialized """
+
+galdos_test = PlaintextCorpusReader('corpus/', 'galdos_test.txt')
+""" The galdos testing data Corpus Reader initialized """
