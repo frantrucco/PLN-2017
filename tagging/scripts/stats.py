2bccf75105306c5a7a504fe81973f6ba7c1cb439
Francisco Trucco <franciscoctrucco@gmail.com>
2017-05-01 23:41:05 -0300

    Add some stats to stats script

--- tagging/scripts/stats.py
+++ tagging/scripts/stats.py
@@ -5,11 +5,22 @@ Usage:
   stats.py -h | --help
 
 Options:
-  -h --help     Show this screen.
+  -h --help     this Show screen.
 """
 from docopt import docopt
 
+from collections import defaultdict
 from corpus.ancora import SimpleAncoraCorpusReader
+from tabulate import tabulate
+
+def print_table(table, headers=None):
+    tablefmt = 'fancy_grid'
+    if headers is None:
+        t = tabulate(table, tablefmt=tablefmt)
+    else:
+        t = tabulate(table, headers, tablefmt=tablefmt)
+
+    print(t)
 
 
 if __name__ == '__main__':
@@ -19,5 +30,19 @@ if __name__ == '__main__':
     corpus = SimpleAncoraCorpusReader('ancora/ancora-2.0/')
     sents = list(corpus.tagged_sents())
 
-    # compute the statistics
-    print('sents: {}'.format(len(sents)))
+    vocabulary = defaultdict(int)
+    tag_vocabulary = defaultdict(int)
+
+    for sent in sents:
+        for token, tag in sent:
+            vocabulary[token] += 1
+            tag_vocabulary[tag] += 1
+
+    table = [
+        ['Number of Sentences', len(sents)],
+        ['Number of tokens', sum(map(len, sents))],
+        ['Vocabulary size ', len(vocabulary)],
+        ['Tag Vocabulary size', len(tag_vocabulary)],
+    ]
+
+    print_table(table, None)
