02514e0139e9bb6ef36bc07e11b6a8068f813966
Francisco Trucco <franciscoctrucco@gmail.com>
2017-05-10 16:50:43 -0300

    Implement baseline model

--- tagging/baseline.py
+++ tagging/baseline.py
@@ -1,3 +1,12 @@
+from collections import defaultdict
+
+
+# Better name than itemgetter(0)
+def first(x): return x[0]
+
+
+# Better name than itemgetter(1)
+def second(x): return x[1]
 
 
 class BaselineTagger:
@@ -6,25 +15,44 @@ class BaselineTagger:
         """
         tagged_sents -- training sentences, each one being a list of pairs.
         """
-        pass
+        sents = tagged_sents
+        tag_per_word = defaultdict(lambda: defaultdict(int))
+        word_per_tag = defaultdict(list)
+
+        self.most_frequent_tag = most_frequent_tag = {}
+
+        for sent in sents:
+            for token, tag in sent:
+                tag_per_word[token][tag] += 1
+                word_per_tag[tag].append(token)
+
+        for word, tag_freqs in tag_per_word.items():
+            # Sort the tags per word by decreasing frequency
+            sorted_tag_freqs = sorted(tag_freqs.items(), key=second,
+                                      reverse=True)
+
+            most_frequent_tag[word] = first(sorted_tag_freqs[0])
 
     def tag(self, sent):
         """Tag a sentence.
 
         sent -- the sentence.
         """
-        return [self.tag_word(w) for w in sent]
+        return [self.tag_word(word) for word in sent]
 
-    def tag_word(self, w):
+    def tag_word(self, word):
         """Tag a word.
 
-        w -- the word.
+        word -- the word.
         """
-        return 'nc'
+        if self.unknown(word):
+            return 'nc0s000'
+        else:
+            return self.most_frequent_tag[word]
 
-    def unknown(self, w):
+    def unknown(self, word):
         """Check if a word is unknown for the model.
 
-        w -- the word.
+        word -- the word.
         """
-        return True
+        return word not in self.most_frequent_tag
