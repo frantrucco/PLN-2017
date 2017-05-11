53f42cf245da063dcb51ff1a847cb7c2a8c810d0
Francisco Trucco <franciscoctrucco@gmail.com>
2017-05-11 19:44:53 -0300

    Implement Viterbi algorithm

--- tagging/hmm.py
+++ tagging/hmm.py
@@ -134,7 +134,7 @@ class HMM:
 
         sent -- the sentence.
         """
-        pass
+        return ViterbiTagger(self).tag(sent)
 
 
 class ViterbiTagger:
@@ -143,9 +143,74 @@ class ViterbiTagger:
         """
         hmm -- the HMM.
         """
+        self.hmm = hmm
 
     def tag(self, sent):
         """Returns the most probable tagging for a sentence.
 
         sent -- the sentence.
         """
+        hmm = self.hmm
+        n = hmm.n
+        inf = float('inf')
+        tagset = hmm.tagset
+
+        self._pi = pi = defaultdict(lambda: defaultdict(tuple))
+
+        opening_tags = ('<s>',) * (n - 1)
+
+        # Initialize table in base cases
+        pi[0][opening_tags] = (0.0, [])
+
+        for k, word in enumerate(sent):
+            for prev_tags in pi[k]:
+                for t in tagset:
+                    out = hmm.log_out_prob(word, t)
+                    trans = hmm.log_trans_prob(t, prev_tags)
+
+                    # If the probability is 0 it is not necessary to add it to
+                    # pi.
+                    if out == -inf or trans == -inf:
+                        continue
+
+                    # The probability it is not 0
+
+                    # The maximum probability of a tag sequence ending in tags
+                    # prev_tags at position k and the path (sequence of tags)
+                    # that results in that maximum probability.
+                    max_prob_k, max_path_k = pi[k][prev_tags]
+
+                    prob = out + trans + max_prob_k
+
+                    tags = (prev_tags + (t,))[1:]  # t_{n - 1} ... t_2 t
+
+                    # The recursive formula is:
+                    # maximum value for every possible t in tagset of:
+                    # out(word, t) + trans(t, prev_tags) + max_prob_k
+                    # To obtain this we use pi[k+1][tags] as a placeholder for
+                    # the maximum value, i.e., the value of pi[k+1][tags] will
+                    # always contain the maximum known probability and path.
+                    # When a higher value is obtained by using a different tag,
+                    # the value of pi[k+1][tags] will be changed accordingly.
+
+                    if tags in pi[k + 1]:
+                        # There is a previous max probability in pi[k+1][tags]
+                        current_max_prob, _ = pi[k + 1][tags]
+                    else:
+                        # This is the first time we set pi[k+1][tags]
+                        current_max_prob = -inf
+
+                    if prob > current_max_prob:
+                        # Change the value of the probability saved in
+                        # pi[k+1][tags] given that we have found a path with a
+                        # higher probability.
+                        pi[k + 1][tags] = (prob, max_path_k + [t])
+
+        # Return the path with highest probability from the start of the sent
+        # to the end of it.
+        k = len(sent)
+        prob, path = max(pi[k].values())
+
+        # If we wanted to return the probability of the tagging sentence we
+        # would need to multiply prob by log_trans_prob('</s>', path[-n:])
+        return path
