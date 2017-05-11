e57a4f4ff24748919b04f79d974c9a8acacd05c6
Francisco Trucco <franciscoctrucco@gmail.com>
2017-05-11 16:09:12 -0300

    Refactor HMM class and fix bugs related to underflow using a new log
    log2 from math underflows when x equals 0. New custom log2 returns -inf when x
    equals 0.

--- tagging/hmm.py
+++ tagging/hmm.py
@@ -1,4 +1,11 @@
-from math import log2
+from collections import defaultdict
+from math import log
+
+
+def log2(x):
+    if x == 0:
+        return float('-inf')
+    return log(x, 2)
 
 
 class HMM:
@@ -10,10 +17,16 @@ class HMM:
         trans -- transition probabilities dictionary.
         out -- output probabilities dictionary.
         """
+        def to_default_dict(d):
+            result = defaultdict(lambda: defaultdict(float))
+            for k, v in d.items():
+                result[k] = defaultdict(float, v)
+            return result
+
         self.n = n
         self.tagset = tagset
-        self.trans = trans
-        self.out = out
+        self.out = to_default_dict(out)
+        self.trans = to_default_dict(trans)
 
     def tagset(self):
         """Returns the set of tags.
@@ -28,6 +41,14 @@ class HMM:
         """
         return self.trans[tuple(prev_tags)][tag]
 
+    def log_trans_prob(self, tag, prev_tags):
+        """Log probability of a tag.
+
+        tag -- the tag.
+        prev_tags -- iterable with the previous n-1 tags
+        """
+        return log2(self.trans_prob(tag, prev_tags))
+
     def out_prob(self, word, tag):
         """Probability of a word given a tag.
 
@@ -35,6 +56,14 @@ class HMM:
         tag -- the tag.
         """
         return self.out[tag][word]
+
+    def log_out_prob(self, word, tag):
+        """Log probability of a word given a tag.
+
+        word -- the word.
+        tag -- the tag.
+        """
+        return log2(self.out_prob(word, tag))
 
     def add_opening_and_closing_tags(self, y):
         """Add both opening and closing tags to a list of tags or words
@@ -59,16 +88,16 @@ class HMM:
             prob *= self.trans_prob(tag, prev_tags)
         return prob
 
-    def prob(self, x, y):
+    def prob(self, word, y):
         """
         Joint probability of a sentence and its tagging.
         Warning: subject to underflow problems.
 
-        x -- sentence.
+        word -- sentence.
         y -- tagging.
         """
         prob = self.tag_prob(y)
-        for word, tag in zip(x, y):
+        for word, tag in zip(word, y):
             prob *= self.out_prob(word, tag)
         return prob
 
@@ -85,19 +114,19 @@ class HMM:
         for i in range(n - 1, len(y)):
             tag = y[i]
             prev_tags = y[i - n + 1:i]
-            prob += log2(self.trans_prob(tag, prev_tags))
+            prob += self.log_trans_prob(tag, prev_tags)
         return prob
 
-    def log_prob(self, x, y):
+    def log_prob(self, word, y):
         """
         Joint log-probability of a sentence and its tagging.
 
-        x -- sentence.
+        word -- sentence.
         y -- tagging.
         """
         prob = self.tag_log_prob(y)
-        for word, tag in zip(x, y):
-            prob += log2(self.out_prob(word, tag))
+        for word, tag in zip(word, y):
+            prob += self.log_out_prob(word, tag)
         return prob
 
     def tag(self, sent):
