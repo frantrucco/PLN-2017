09faa390b0e198db7dc99434a57dfe10817a2eb1
Francisco Trucco <franciscoctrucco@gmail.com>
2017-05-17 21:17:00 -0300

    Implement features.py

--- tagging/features.py
+++ tagging/features.py
@@ -14,5 +14,81 @@ def word_lower(h):
 
     h -- a history.
     """
-    sent, i = h.sent, h.i
-    return sent[i].lower()
+    if h.i < 0:
+        return 'BOS'
+    return h.sent[h.i].lower()
+
+
+def word_istitle(h):
+    """Feature: is current word a title?
+
+    h -- a history.
+    """
+    if h.i < 0:
+        return 'BOS'
+    return h.sent[h.i].istitle()
+
+
+def word_isupper(h):
+    """Feature: is current word uppercase?
+
+    h -- a history.
+    """
+    if h.i < 0:
+        return 'BOS'
+    return h.sent[h.i].isupper()
+
+
+def word_isdigit(h):
+    """Feature: is current word a digit?
+
+    h -- a history.
+    """
+    if h.i < 0:
+        return 'BOS'
+    return h.sent[h.i].isdigit()
+
+
+def prev_tags(h):
+    """Feature: the previous tags of current word
+
+    h -- a history.
+    """
+    return h.prev_tags
+
+
+class NPrevTags(Feature):
+    def __init__(self, n):
+        """Feature: n previous tags tuple.
+
+        n -- number of previous tags to consider.
+        """
+        assert n > 0
+        self.n = n
+
+    def _evaluate(self, h):
+        """n previous tags tuple.
+
+        h -- a history.
+        """
+        return h.prev_tags[-self.n:]  # Given that n > 0 we can use this
+
+
+class PrevWord(Feature):
+
+    def __init__(self, f):
+        """Feature: the feature f applied to the previous word.
+
+        f -- the feature.
+        """
+        self.f = f
+
+    def _evaluate(self, h):
+        """Apply the feature to the previous word in the history.
+
+        h -- the history.
+        """
+        if h.i < 0:
+            return str('BOS')
+        history = History(h.sent, h.prev_tags, h.i - 1)
+        return str(self.f(history))
