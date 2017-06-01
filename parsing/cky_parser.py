1a8878b8475d6aae9ce5315aee0a79dd8cf1dee4
Francisco Trucco <franciscoctrucco@gmail.com>
2017-06-01 18:03:16 -0300

    Implement CKYParser
    Note that the complexity is O(n**3 * G) where G is the grammar size. There is
    another implementation with a worse time complexity ( O(n**3 * G ** 2) ) that
    calculates the cartesian product between the symbols (nonterminals and
    terminals) on the bottom right interval and on the bottom left interval of the
    current cell and then filters that cartesian product discarding all pairs (B, C)
    that do not belong to any production in the grammar.

---
+++ parsing/cky_parser.py
@@ -0,0 +1,94 @@
+from collections import defaultdict
+from nltk.tree import Tree
+
+
+class CKYParser:
+
+    def __init__(self, grammar):
+        """
+        grammar -- a binarised NLTK PCFG.
+        """
+        self.grammar = grammar
+        self.from_rhs = defaultdict(list)
+        for p in grammar.productions():
+            lhs, rhs, prob = CKYParser.prod_to_triple(p)
+            self.from_rhs[rhs].append((lhs, prob))
+
+    def prod_to_triple(p):
+        """Convert the production p to a triple of the form (lhs, rhs, prob)
+
+        Additionally:
+            - lhs is converted to a string
+            - each element in rhs is converted to a string
+
+        p -- the production rule
+        """
+        return (str(p.lhs()), tuple(map(str, p.rhs())), p.logprob())
+
+    def binary_prods(self, Bs, Cs):
+        """Return a list of 4-tuples of the form (A, B, C, probability of rule)
+
+        Create a list of 4-tuples containing all rules of the from A -> B C
+        where B is in Bs and C is in Cs.
+
+        Bs -- list of terminal or nonterminals.
+        Cs -- list of terminal or nonterminals.
+        """
+        from_rhs = self.from_rhs
+
+        binary_prods = []
+        for rhs in from_rhs.keys():
+            if len(rhs) != 2:
+                continue
+            B, C = rhs
+            binary_prods += [(A, B, C, prob)
+                             for A, prob in from_rhs[rhs]
+                             if B in Bs and C in Cs]
+        return binary_prods
+
+    def parse(self, sent):
+        """Parse a sequence of terminals.
+
+        sent -- the sequence of terminals.
+        """
+        n = len(sent)
+        inf = float('inf')
+        self._pi = score = {}
+        self._bp = back = {}
+        for i in range(1, n + 1):
+            for j in range(i, n + 1):
+                score[(i, j)] = {}
+                back[(i, j)] = {}
+
+        from_rhs = self.from_rhs
+        for i, word in enumerate(sent):
+            for A, prob in from_rhs[(word,)]:
+                j = i + 1
+                score[(j, j)][A] = prob
+                back[(j, j)][A] = Tree(A, [word])
+
+        for span in range(1, n):
+            for begin in range(1, n - span + 1):
+                end = begin + span
+                whole = (begin, end)  # The entire interval
+                for split in range(begin, end):
+                    left, right = (begin, split), (split + 1, end)
+
+                    # All binary productions of the form A -> B C where
+                    # B is in score[left] and C is in score[right]
+                    binary_prods = self.binary_prods(score[left], score[right])
+
+                    for A, B, C, rule_prob in binary_prods:
+                        prob = score[left][B]
+                        prob += score[right][C]
+                        new_prob = prob + rule_prob
+                        if A not in score[whole] or score[whole][A] < new_prob:
+                            score[whole][A] = new_prob
+                            back[whole][A] = Tree(A, [back[left][B],
+                                                      back[right][C]])
+
+        start = str(self.grammar.start())
+        whole = (1, n)  # The entire sentence
+        if whole in score and start in score[whole]:
+            return (score[whole][start], back[whole][start])
+        return (-inf, None)
